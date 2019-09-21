6fb23fa2f6cec895bd2fea2df5c6299a6ebb62bb
root <root@tilde.club>
2019-09-21 14:52:11 -0400

    use relative links and correct css

--- header.php
+++ header.php
@@ -4,12 +4,14 @@
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title><?=isset($title) ? $title : "Welcome to ~tilde.club~"?></title>
-		<link rel="stylesheet" href="https://yourtilde.com/~deepend/tcnew/style.css">
+		<link rel="stylesheet" href="/style.css">
 
 	</head>
 <body>
  <div id="navbar">
-  <a href="https://tilde.club/">HOME</a>
-  <a href="https://tilde.club/wiki">WIKI</a>
-  <a href="https://tilde.club/signup">SIGN-UP</a>
+  <a href="/">~TILDE.CLUB~</a>
+  <a href="/">HOME</a>
+  <a href="/wiki/">WIKI</a>
+  <a href="/signup/">SIGN-UP</a>
 </div> 
+
