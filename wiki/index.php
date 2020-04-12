ea181def2fab066dce5df6bb75866365572bd117
Travis Briggs <audiodude@gmail.com>
2020-04-12 01:09:33 -0700

    Sort wiki articles by actual title instead of filename

--- wiki/index.php
+++ wiki/index.php
@@ -7,14 +7,18 @@ include __DIR__."/../header.php";
 
 <p>here's the articles on our wiki:</p>
 <ul>
-	<?php foreach (glob("source/*.md") as $file) {
-		$article = basename($file, ".md"); 
-		$title = preg_match("/title: (.*)/i", file_get_contents($file), $matches) 
-			? $matches[1] : $article;
-	?>
+        <?php 
+              $article_to_title = [];
+              foreach (glob("source/*.md") as $file) {
+                $article = basename($file, ".md");
+		$title = preg_match("/title: (.*)/i", file_get_contents($file), $matches) ? $matches[1] : $article;
+                $article_to_title[$article] = $title;
+              }
+              sort($article_to_title);
+        ?>
+	<?php foreach ($article_to_title as $article => $title)	{ ?>
 		<li><a href="/wiki/<?=$article?>.html"><?=$title?></a></li>
 	<?php } ?>
 </ul>
 
 <?php include __DIR__."/../footer.php";
-
