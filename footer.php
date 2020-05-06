67574bdb60d28bb8dade488fd4ac23353d3cc768
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2020-05-05 23:10:02 -0600

    Remove banner code.

--- footer.php
+++ footer.php
@@ -1,9 +1,6 @@
 
 	</div>
 	<br>
-<!-- Tildeverse Banner code begin -->
-<div style="text-align: center;"><object data="https://banner.tildeverse.org/work.php?ID=deepend" type="text/html" width="500" height="80" style="overflow:hidden; width: 500px; height: 80px"></object></div>
-<!-- Tildeverse Banner code end -->
 	</body>
 </html>
 
