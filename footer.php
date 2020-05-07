c7248a0ea31edc907dce4a6f1c72342b584f1457
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2020-05-06 20:59:11 -0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,5 +2,9 @@
 	</div>
 	<br>
 	</body>
+        <footer class="text-center">
+            <p>ECDSA fingerprint: </p>
+            <a href="https://github.com/tildeclub/site">page source</a>
+        </footer>
 </html>
 
