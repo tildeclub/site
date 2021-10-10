09459d976dd63882f582fe317dca8e9c72ad1445
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2021-10-10 16:21:19 -0600

    added ~dmi3 to gold star supporters

--- index.php
+++ index.php
@@ -64,6 +64,7 @@
             Here's who has donated! When you're on the
             server, THANK THEM.</p>
             <ul>
+                <li>10/10/2021 | <a href="/~dmi3">~dmi3</a></li>
                 <li>03/08/2021 | <a href="/~webwiz">~webwiz</a></li>
                 <li>02/13/2021 | <a href="/~whitcomb">~whitcomb</a></li>
                 <li>11/30/2020 | <a href="/~dethelor">~dethelor</a></li>
