0ffeb101ee7839574315790a03bae31fa0fdba03
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2022-04-14 17:09:28 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <p><a href="/wiki/faq.html">Questions? See the official FAQ.</a></p>
 
-<a href="https://donate.redcross.ca/page/100227/donate/1?_gl=1*li75g9*_ga*MTYxMTE4MjA2Ni4xNjQ0NjE1NDcw*_ga_376D8LHM0R*MTY0NTc5MzU5MC4xMi4wLjE2NDU3OTM1OTAuMA..&_ga=2.8498675.712879>
+<a href="https://donate.redcross.ca/page/100227/donate/1?_gl=1*li75g9*_ga*MTYxMTE4MjA2Ni4xNjQ0NjE1NDcw*_ga_376D8LHM0R*MTY0NTc5MzU5MC4xMi4wLjE2NDU3OTM1OTAuMA..&_ga=2.8498675.712879640.1645730482-1611182066.1644615470" target="_blank"><img src="images/StandWithUkraine.svg"></a>
 <br>
 <br>
 <div class="grid">
