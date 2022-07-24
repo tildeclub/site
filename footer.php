7a6a0f9374b6a589e39790460887e140bd87c41d
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2022-07-24 16:32:33 -0600

    added status link to footer.

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
 	<br>
         <footer class="text-center">
             <p>ECDSA fingerprint: SHA256:duamOATgnGcfRFFkotCwrAWzZtRjwxm64WAhq5tQRwE</p>
-            <a href="https://github.com/tildeclub/site">page source</a>
+            <a href="https://github.com/tildeclub/site">page source</a> | <a href="https://status.tilde.one/status/tildeclub">Services Status</a>
         </footer>
 	</body>
 </html>
