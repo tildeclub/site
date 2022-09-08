e52ead3ab440341ed4f0207b0d54b2a815281f66
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2022-09-08 11:02:02 -0600

    added gold star supporter.

--- index.php
+++ index.php
@@ -27,6 +27,7 @@
             Here's who has donated! When you're on the
             server, THANK THEM.</p>
             <ul>
+                <li>09-08-2022 | <a href="/~cyrus">~cyrus</a></li>
                 <li>06-08-2022 | <a href="/~barnold">~barnold</a></li>
                 <li>05-12-2022 | <a href="/~hifikuno">~hifikuno</a></li>
                 <li>04-27-2022 | <a href="/~mhd">~mhd</a></li>
