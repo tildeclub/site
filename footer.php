a9252d26c6921059010a167a6bb41345bbe96fd1
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2022-12-18 21:39:18 -0700

    update footer.php

--- footer.php
+++ footer.php
@@ -7,6 +7,12 @@
         <footer class="text-center">
             <p>ECDSA fingerprint: SHA256:duamOATgnGcfRFFkotCwrAWzZtRjwxm64WAhq5tQRwE</p>
             <a href="https://github.com/tildeclub/site">page source</a> | <a href="https://status.tilde.one/status/tildeclub">Services Status</a>
+		<br><br>
+<!-- Tildeverse Banner Exchange code begin -->
+	<CENTER>
+		<IFRAME SRC="https://banner.tildeverse.org/work.php?ID=deepend" width=468 height=60 marginwidth=0 marginheight=0 hspace=0 vspace=0 frameborder=0 scrolling="no"></IFRAME>
+	</CENTER>
+<!-- Tildeverse Banner Exchange code begin -->
         </footer>
 	</body>
 </html>
