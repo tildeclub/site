d2b3409a828b8eafb8ee4e5161db2a2f853fd4bd
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2022-12-18 23:34:47 -0700

    update footer.php

--- footer.php
+++ footer.php
@@ -9,6 +9,7 @@
 <!-- Tildeverse Banner Exchange code begin -->
 	<CENTER>
 		<IFRAME SRC="https://banner.tildeverse.org/work.php?ID=deepend" width=468 height=60 marginwidth=0 marginheight=0 hspace=0 vspace=0 frameborder=0 scrolling="no"></IFRAME>
+		<a href="https://banner.tildeverse.org" target="_blank">Tildeverse Banner Exchange</a>
 	</CENTER>
 <!-- Tildeverse Banner Exchange code begin -->
 	<br><br>
