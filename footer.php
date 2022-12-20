799448d08793d6a0a054653c36967c393299f2c6
deepend <deepend@tilde.club>
2022-12-20 00:43:28 +0000

    updated banner code

--- footer.php
+++ footer.php
@@ -7,10 +7,10 @@
         <footer class="text-center">
 	<br><br>
 <!-- Tildeverse Banner Exchange code begin -->
-	<CENTER>
-		<IFRAME SRC="https://banner.tildeverse.org/work.php?ID=deepend" width=468 height=60 marginwidth=0 marginheight=0 hspace=0 vspace=0 frameborder=0 scrolling="no"></IFRAME>
-		<br><a href="https://banner.tildeverse.org" target="_blank">Tildeverse Banner Exchange</a>
-	</CENTER>
+	<div style="text-align: center;">
+		<iframe src="https://banner.tildeverse.org/work.php?ID=deepend" width="468" height="60" marginwidth="0" marginheight="0" hspace="0" vspace="0" frameborder="0" scrolling="no"></iframe>
+		<p><a href="https://banner.tildeverse.org" target="_blank">Tildeverse Banner Exchange</a></p>
+	</div>
 <!-- Tildeverse Banner Exchange code begin -->
 	<br><br>
 	    <p>ECDSA fingerprint: SHA256:duamOATgnGcfRFFkotCwrAWzZtRjwxm64WAhq5tQRwE</p>
