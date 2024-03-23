ccf2ce5d15c7e338b75349acd9f490a690371d9d
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2024-03-23 13:38:35 -0600

    Update index.php

--- signup/index.php
+++ signup/index.php
@@ -2,14 +2,6 @@
 $title = "sign up for the tilde.club!";
 include __DIR__."/../header.php";
 
-<h1 id="fancyboi">sign up to join tilde.club</h1>
-
-<div class="grid">
-    <div class="row">
-        <div class="col">
             <p>Sorry Signups are currently closed.</p>
-
-	</div>
-</div>
 
 <?php include __DIR__."/../footer.php";
