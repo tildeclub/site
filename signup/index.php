5748695869b7fc04a6fb21cc7b05b5baead4f9e8
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2024-03-23 13:36:40 -0600

    Create index.php

---
+++ signup/index.php
@@ -0,0 +1,19 @@
+<?php
+$title = "sign up for the tilde.club!";
+include __DIR__."/../header.php";
+
+<h1 id="fancyboi">sign up to join tilde.club</h1>
+
+<div class="grid">
+    <div class="row">
+        <div class="col">
+            <p>Sorry Signups are currently closed.</p>
+
+		</td>
+	</tr>
+</table>
+
+	</div>
+</div>
+
+<?php include __DIR__."/../footer.php";
