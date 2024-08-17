f17390d706ea394d56e67c8609e005bbfcb366d2
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2024-08-17 11:57:11 -0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,20 +1,20 @@
-
-		</div>
-	</div>
-<div>
-	<br>
-        <footer class="text-center">
-	<br><br>
-<!-- Tildeverse Banner Exchange code begin -->
-	<div style="text-align: center;">
-		<iframe src="https://banner.tildeverse.org/work.php?ID=deepend" width="468" height="60" marginwidth="0" marginheight="0" hspace="0" vspace="0" frameborder="0" scrolling="no"></iframe>
-		<p><a href="https://banner.tildeverse.org" target="_blank">Tildeverse Banner Exchange</a></p>
-	</div>
-<!-- Tildeverse Banner Exchange code begin -->
-	<br><br>
-	    <p>ECDSA fingerprint: SHA256:duamOATgnGcfRFFkotCwrAWzZtRjwxm64WAhq5tQRwE</p>
-		<a href="https://github.com/tildeclub/site">page source</a> | <a href="http://updown.tilde.club/">Uptime Status</a> | <a href="https://tilde.club/stats" target="_blank">Stats</a> | <a href="https://tilde.club/news.php">NNTP Stats</a>
-	</footer>
-	</body>
+    </div> <!-- Close .content div opened in header.php -->
+</div> <!-- Close the outermost div if there is any -->
+<br>
+<footer class="text-center">
+    <br><br>
+    <!-- Tildeverse Banner Exchange code begin -->
+    <div style="text-align: center;">
+        <iframe src="https://banner.tildeverse.org/work.php?ID=deepend" width="468" height="60" marginwidth="0" marginheight="0" hspace="0" vspace="0" frameborder="0" scrolling="no"></iframe>
+        <p><a href="https://banner.tildeverse.org" target="_blank">Tildeverse Banner Exchange</a></p>
+    </div>
+    <!-- Tildeverse Banner Exchange code end -->
+    <br><br>
+    <p>ECDSA fingerprint: SHA256:duamOATgnGcfRFFkotCwrAWzZtRjwxm64WAhq5tQRwE</p>
+    <a href="https://github.com/tildeclub/site">page source</a> | 
+    <a href="http://updown.tilde.club/">Uptime Status</a> | 
+    <a href="https://tilde.club/stats" target="_blank">Stats</a> | 
+    <a href="https://tilde.club/news.php">NNTP Stats</a>
+</footer>
+</body>
 </html>
-
