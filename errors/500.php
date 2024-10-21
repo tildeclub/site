acdf650fe53846d12401b3fcbda9643f6d86d987
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2024-10-21 11:19:26 -0600

    Create 500.php

---
+++ errors/500.php
@@ -0,0 +1,22 @@
+<?php include "../header.php"; ?>
+
+        <h1 id="fancyboi">ERROR 500 Internal Server Error</h1>
+
+<div class="grid">
+
+		<div class="row">
+
+			<div class="col">
+                        <p>A 500 error implies the server encountered an internal error or misconfiguration and was unable to complete the request.</p>
+			<br>
+		        <h3>If you're a site visitor</h3>
+		        <p>Please use your browser's back button and check that you're in the right place. If you need immediate assistance, please send us an email instead.</p>
+		        <h3>If you're the site owner</h3>
+		        <p>Please check that you're in the right place and get in touch with your website provider if you believe this to be an error.</p>
+
+
+	</div>
+</div>
+
+
+<?php include "../footer.php"; ?>
