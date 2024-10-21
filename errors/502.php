a470a271054053557c01a913be7b760157c0773c
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2024-10-21 11:20:23 -0600

    Create 502.php

---
+++ errors/502.php
@@ -0,0 +1,22 @@
+<?php include "../header.php"; ?>
+
+        <h1 id="fancyboi">ERROR 502 Bad Gateway</h1>
+
+<div class="grid">
+
+		<div class="row">
+
+			<div class="col">
+                        <p>A 502 error implies the server, while acting as a gateway or proxy, received an invalid response from the upstream server.</p>
+			<br>
+		        <h3>If you're a site visitor</h3>
+		        <p>Please use your browser's back button and check that you're in the right place. If you need immediate assistance, please send us an email instead.</p>
+		        <h3>If you're the site owner</h3>
+		        <p>Please check that you're in the right place and get in touch with your website provider if you believe this to be an error.</p>
+
+
+	</div>
+</div>
+
+
+<?php include "../footer.php"; ?>
