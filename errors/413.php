c97eeb3671969a49b54ec9288a4bb6ea896a7ef8
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2024-10-21 10:17:53 -0600

    Create 413.php

---
+++ errors/413.php
@@ -0,0 +1,22 @@
+<?php include "../header.php"; ?>
+
+        <h1 id="fancyboi">ERROR 413 Payload Too Large</h1>
+
+<div class="grid">
+
+		<div class="row">
+
+			<div class="col">
+                        <p>A 413 error implies the server is refusing to process the request because the payload (body of the request) is too large.</p>
+			<br>
+		        <h3>If you're a site visitor</h3>
+		        <p>Please use your browser's back button and check that you're in the right place. If you need immediate assistance, please send us an email instead.</p>
+		        <h3>If you're the site owner</h3>
+		        <p>Please check that you're in the right place and get in touch with your website provider if you believe this to be an error.</p>
+
+
+	</div>
+</div>
+
+
+<?php include "../footer.php"; ?>
