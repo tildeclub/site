79e32499b53ceb40ad61cf59c232978c0d1c49b5
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2024-10-21 10:15:47 -0600

    Create 405.php

---
+++ errors/405.php
@@ -0,0 +1,22 @@
+<?php include "../header.php"; ?>
+
+        <h1 id="fancyboi">ERROR 405 Method Not Allowed</h1>
+
+<div class="grid">
+
+		<div class="row">
+
+			<div class="col">
+                        <p>A 405 error implies the method used in the request (such as GET, POST, PUT, etc.) is not allowed for the requested resource.</p>
+			<br>
+		        <h3>If you're a site visitor</h3>
+		        <p>Please use your browser's back button and check that you're in the right place. If you need immediate assistance, please send us an email instead.</p>
+		        <h3>If you're the site owner</h3>
+		        <p>Please check that you're in the right place and get in touch with your website provider if you believe this to be an error.</p>
+
+
+	</div>
+</div>
+
+
+<?php include "../footer.php"; ?>
