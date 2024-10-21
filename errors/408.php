3a4823243e472224e2fbc699153e3563675b775f
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2024-10-21 10:16:36 -0600

    Create 408.php

---
+++ errors/408.php
@@ -0,0 +1,22 @@
+<?php include "../header.php"; ?>
+
+        <h1 id="fancyboi">ERROR 408 Request Timeout</h1>
+
+<div class="grid">
+
+		<div class="row">
+
+			<div class="col">
+                        <p>A 408 error implies the server timed out while waiting for the client’s request. This usually happens when the request takes too long to complete.</p>
+			<br>
+		        <h3>If you're a site visitor</h3>
+		        <p>Please use your browser's back button and check that you're in the right place. If you need immediate assistance, please send us an email instead.</p>
+		        <h3>If you're the site owner</h3>
+		        <p>Please check that you're in the right place and get in touch with your website provider if you believe this to be an error.</p>
+
+
+	</div>
+</div>
+
+
+<?php include "../footer.php"; ?>
