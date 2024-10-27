8fe11a8dbae0c1facb91c9670acfc0d533d83a3f
litemotiv <litemotiv@tilde.club>
2024-10-27 11:54:51 +0100

    Remove legacy IE tag

--- header.php
+++ header.php
@@ -2,7 +2,6 @@
 <html dir="ltr" lang="en">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?=isset($title) ? $title : "Welcome to ~tilde.club~"?></title>
     
