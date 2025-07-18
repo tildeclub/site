bb5886b4770f4844bf96f56a7ac87bf205ed2b0f
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2025-07-18 13:29:18 -0600

    Update header24h.php
    new donate page

--- header24h.php
+++ header24h.php
@@ -13,7 +13,7 @@
 		<a href="/">HOME</a>
 		<a href="/wiki/">WIKI</a>
 		<a href="https://tildeforge.dev" target="_blank">TILDEFORGE</a>
-		<a href="https://tilde.club/wiki/donate.html">DONATE</a>
+		<a href="https://donate.tilde.club">DONATE</a>
 		<a href="https://web.newnet.net/?join=club" target="_blank">CHAT</a>
 		<a href="/signup/">SIGN-UP</a>
 	</div>
