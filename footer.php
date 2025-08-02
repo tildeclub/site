bbbaa2de7fb1da2f7778927c87c3fd4b9f3a5116
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2025-08-02 11:57:16 -0600

    Update footer.php
    remove status page and updated banner code.

--- footer.php
+++ footer.php
@@ -5,14 +5,13 @@
     <br><br>
     <!-- Tildeverse Banner Exchange code begin -->
     <div style="text-align: center;">
-        <iframe src="https://banner.tildeverse.org/work.php?ID=deepend" width="468" height="60" marginwidth="0" marginheight="0" hspace="0" vspace="0" frameborder="0" scrolling="no"></iframe>
-        <p><a href="https://banner.tildeverse.org" target="_blank">Tildeverse Banner Exchange</a></p>
+        <iframe src="https://tildeexchange.org/work.php?ID=deepend" width="468" height="60" marginwidth="0" marginheight="0" hspace="0" vspace="0" frameborder="0" scrolling="no"></iframe>
+        <p><a href="https://tildeexchange.org" target="_blank">Tildeverse Banner Exchange</a></p>
     </div>
     <!-- Tildeverse Banner Exchange code end -->
     <br><br>
     <p>ECDSA fingerprint: SHA256:duamOATgnGcfRFFkotCwrAWzZtRjwxm64WAhq5tQRwE</p>
     <a href="https://github.com/tildeclub/site">page source</a> | 
-    <a href="http://updown.tilde.club/">Uptime Status</a> | 
     <a href="https://tilde.club/stats" target="_blank">Stats</a> | 
     <a href="https://tilde.club/news.php">NNTP Stats</a>
 </footer>
